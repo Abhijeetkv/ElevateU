75f33c1ab6b89314f8db112edcb6720974ebed1f
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-09-01 17:26:04 +0530

    feat: Update package.json and package-lock.json with new dependencies including humanize-duration, quill, rc-progress, react-router-dom, react-simple-star-rating, react-youtube, and uniqid
    feat: Implement routing in App component with new pages for Home, CourseList, CourseDetails, MyEnrollment, Player, and Loading
    
    feat: Add new components for educator and student pages including Footer, Navbar, Sidebar, CallToAction, Companies, CourseCard, CoursesSection, Hero, Loading, Mycourses, StudentEnrolled, and Rating
    
    feat: Create context for AppContext to manage global state
    
    style: Update index.css to include Google Fonts and apply font-family to all elements
    
    feat: Add new pages for educator and student functionalities including AddCourse, Dashboard, Educator, CourseDetails, CoursesList, Home, MyEnrollment, and Player

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/student/Home'
+import CourseList from './pages/student/CoursesList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollment from './pages/student/MyEnrollment'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/course-list/:input" element={<CourseList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollment />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+      </Routes>
     </>
   )
 }
