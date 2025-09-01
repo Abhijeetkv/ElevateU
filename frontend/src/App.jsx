807df8a7e07052d04fc20d596f61dbc491ebdeb3
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-09-01 17:55:32 +0530

    feat: Add educator routes and components for Dashboard, AddCourse, MyCourses, and StudentEnrolled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import CourseDetails from './pages/student/CourseDetails'
 import MyEnrollment from './pages/student/MyEnrollment'
 import Player from './pages/student/Player'
 import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import StudentEnrolled from './pages/educator/StudentEnrolled'
+import MyCourses from './pages/educator/Mycourses'
 
 function App() {
 
@@ -18,6 +23,12 @@ function App() {
         <Route path="/my-enrollments" element={<MyEnrollment />} />
         <Route path="/player" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />} >
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentEnrolled />} />
+        </Route>
       </Routes>
     </>
   )
