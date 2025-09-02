a3082d1b9e7387eb5ec15091a55f5972953a685b
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-09-03 00:41:12 +0530

    feat: Implement Companies component and integrate SearchBar in Hero; update Home page layout

--- frontend/src/pages/student/Home.jsx
+++ frontend/src/pages/student/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../components/student/Hero.jsx'
+import Companies from '../../components/student/Companies.jsx'
 
 const Home = () => {
   return (
     <>
     <div className='flex flex-col item-center space-y-7 text-center'>
       <Hero />
+      <Companies />
     </div>
     </>
   )
