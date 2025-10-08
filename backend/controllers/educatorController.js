a5b8751505f82fdd9afeaf279233c223792f3dbb
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-10-08 23:22:19 +0530

    refactor: Improve code formatting and consistency in educatorController.js

--- backend/controllers/educatorController.js
+++ backend/controllers/educatorController.js
@@ -1,158 +1,162 @@
-import {clerkClient} from '@clerk/express'
-import Course from '../models/Course.js'
-import {v2 as cloudinary} from 'cloudinary'
-import Purchase from '../models/Purchase.js'
-import User from '../models/User.js'
+import { clerkClient } from "@clerk/express";
+import Course from "../models/Course.js";
+import { v2 as cloudinary } from "cloudinary";
+import Purchase from "../models/Purchase.js";
+import User from "../models/User.js";
 
 export const updateRoleToEducator = async (req, res) => {
-    try {
-        const userId = req.auth.userId
+  try {
+    const userId = req.auth.userId;
 
-        await clerkClient.users.updateUserMetadata(userId, {
-            publicMetadata:{
-                role: 'educator'
-            }
-        })
-        res.json({
-            success: true,
-            message: 'You can publish a course now'
-        })
-    } catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+    await clerkClient.users.updateUserMetadata(userId, {
+      publicMetadata: {
+        role: "educator",
+      },
+    });
+    res.json({
+      success: true,
+      message: "You can publish a course now",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //  Add new course
 export const addCourse = async (req, res) => {
-    try {
-        const {courseData} = req.body
-        const imageFile = req.file
-        const educatorId = req.auth.userId
+  try {
+    const { courseData } = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
 
-        if(!imageFile){
-            return res.json({
-                success: false,
-                message: 'Image file is required'
-            })
-        }
-
-        const parsedCourseData = JSON.parse(courseData)
-        parsedCourseData.educator = educatorId
-        const newCourse = await Course.create(parsedCourseData)
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
-        newCourse.courseThumbnail = imageUpload.secure_url
-        await newCourse.save()
-
-        res.json({
-            success: true,
-            message: 'Course created successfully',
-        })
-
-    } catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
+    if (!imageFile) {
+      return res.json({
+        success: false,
+        message: "Image file is required",
+      });
     }
-}
 
+    const parsedCourseData = JSON.parse(courseData);
+    parsedCourseData.educator = educatorId;
+    const newCourse = await Course.create(parsedCourseData);
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+    newCourse.courseThumbnail = imageUpload.secure_url;
+    await newCourse.save();
+
+    res.json({
+      success: true,
+      message: "Course created successfully",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Get educator courses
 export const getEducatorCourses = async (req, res) => {
-    try {
-        const educatorId = req.auth.userId
-        const courses = await Course.find({ educator: educatorId })
-        res.json({
-            success: true,
-            data: courses
-        })
-    } catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+  try {
+    const educatorId = req.auth.userId;
+    const courses = await Course.find({ educator: educatorId });
+    res.json({
+      success: true,
+      data: courses,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // get educator dashboard data (Total Earning, Enrolled Students, No. of courses)
 export const getEducatorDashboardData = async (req, res) => {
-    try {
-        const educatorId = req.auth.userId
-        const courses = await Course.find({ educator: educatorId })
-        const totalCourses = courses.length
+  try {
+    const educatorId = req.auth.userId;
+    const courses = await Course.find({ educator: educatorId });
+    const totalCourses = courses.length;
 
-        const courseIds = courses.map(course => course._id)
+    const courseIds = courses.map((course) => course._id);
 
-        // Calculate total earnings from purchases
-        const purchases = await Purchase.find({
-            courseId: { $in: courseIds },
-            status: 'completed'
-        })
+    // Calculate total earnings from purchases
+    const purchases = await Purchase.find({
+      courseId: { $in: courseIds },
+      status: "completed",
+    });
 
-        const totalEarnings = purchases.reduce((sum, purchase) => sum + purchase.amount, 0)
+    const totalEarnings = purchases.reduce(
+      (sum, purchase) => sum + purchase.amount,
+      0
+    );
 
-        // collect unique enrolled students ids with their course titles
-        const enrolledStudentData = [];
-        for(const course of courses){
-            const students = await User.find({
-                _id: { $in: course.enrolledStudents }
-            }, 'name imageUrl')
+    // collect unique enrolled students ids with their course titles
+    const enrolledStudentData = [];
+    for (const course of courses) {
+      const students = await User.find(
+        {
+          _id: { $in: course.enrolledStudents },
+        },
+        "name imageUrl"
+      );
 
-            students.forEach(student => {
-                enrolledStudentData.push({
-                    courseTitle: course.courseTitle,
-                    student
-                })
-            })
-        }
-
-        res.json({
-            success: true,
-            dashboardData: {
-                totalCourses,
-                totalEarnings,
-                enrolledStudentData
-            }
-        })
-    } catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
+      students.forEach((student) => {
+        enrolledStudentData.push({
+          courseTitle: course.courseTitle,
+          student,
+        });
+      });
     }
-}
 
+    res.json({
+      success: true,
+      dashboardData: {
+        totalCourses,
+        totalEarnings,
+        enrolledStudentData,
+      },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //  Get Enrolled Students Data with Purchase Data
 export const getEnrolledStudentsData = async (req, res) => {
-    try {
-        const educatorId = req.auth.userId
-        const courses = await Course.find({ educator: educatorId })
-        const courseIds = courses.map(course => course._id)
+  try {
+    const educatorId = req.auth.userId;
+    const courses = await Course.find({ educator: educatorId });
+    const courseIds = courses.map((course) => course._id);
 
+    const purchases = await Purchase.find({
+      courseId: { $in: courseIds },
+      status: "completed",
+    })
+      .populate("userId", "name imageUrl")
+      .populate("courseId", "courseTitle");
 
-            const purchases = await Purchase.find({
-                courseId: { $in: courseIds },
-                status: 'completed'      
-            }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+    const enrolledStudents = purchases.map((purchase) => ({
+      student: purchase.userId,
+      courseTitle: purchase.courseId.courseTitle,
+      purchaseDate: purchase.createdAt,
+    }));
 
-            const enrolledStudents = purchases.map(purchase => ({
-                student: purchase.userId,
-                courseTitle: purchase.courseId.courseTitle,
-                purchaseDate: purchase.createdAt,
-            }))
-
-            res.json({
-                success: true,
-                enrolledStudents
-            })
-    } catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+    res.json({
+      success: true,
+      enrolledStudents,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
