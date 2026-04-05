f5fd9adce8d36d19e2228ea02f649016e802d47c
Abhijeetkv <abhijeetkv2@gmail.com>
2026-04-06 00:14:54 +0530

    added clean ui

--- frontend/src/pages/student/Home.jsx
+++ frontend/src/pages/student/Home.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
 import Hero from '../../components/student/Hero.jsx'
-import Companies from '../../components/student/Companies.jsx'
 import CoursesSection from '../../components/student/CoursesSection.jsx'
-import TestimonialSec from '../../components/student/TestimonialSec.jsx'
 import CallToAction from '../../components/student/CallToAction.jsx'
 import Footer from '../../components/student/Footer.jsx'
 
@@ -11,9 +8,7 @@ const Home = () => {
     <>
     <div className='flex flex-col item-center space-y-7 text-center'>
       <Hero />
-      <Companies />
       <CoursesSection />
-      <TestimonialSec />
       <CallToAction />
       <Footer />
     </div>
